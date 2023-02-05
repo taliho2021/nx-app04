4151350e11b4f6edaeea7058f070a91f7019fe84
Tal Yo <tal@analink.com>
2023-02-05 12:17:13 -0500

    Initial commit

---
+++ apps/first/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-app04-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'first';
+}
