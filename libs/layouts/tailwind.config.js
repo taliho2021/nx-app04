79222a70c8a3c18b5b9b6d4ffe28fe837cff4797
taliho2021 <tal@analink.com>
2023-02-07 17:14:25 -0500

    Updated main component

--- libs/layouts/tailwind.config.js
+++ libs/layouts/tailwind.config.js
@@ -2,15 +2,24 @@ const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sky:  colors.sky,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        rose: colors.rose,
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/forms')
-  ],
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp')],
 };
