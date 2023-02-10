9d4db1dad80ffcc26d08886f698a31303aeb3ce2
Tal Yo <tal@analink.com>
2023-02-10 08:56:21 -0500

    added address component from schematic

--- libs/parties/src/index.ts
+++ libs/parties/src/index.ts
@@ -4,8 +4,10 @@ export * from './lib/lib.routes';
 
 export * from './lib/parties/carriers/carriers.component';
 
-// export * from './lib/parties/clients/clients.component';
+export * from './lib/parties/clients/clients.component';
 
-// export * from './lib/parties/manufacturers/manufacturers.component';
+export * from './lib/parties/manufacturers/manufacturers.component';
 
-// export * from './lib/parties/forwarders/forwarders.component';
+export * from './lib/parties/forwarders/forwarders.component';
+
+export * from './lib/parties/address/address.component';
